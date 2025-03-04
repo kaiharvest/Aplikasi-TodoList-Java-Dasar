79e8d434dfbbbc3e43e60b87b7f40520fafa0b5a
indra <indradwiprabowo27@gmail.com>
2025-03-04 11:19:40 +0700

    commit

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,41 @@
+public class AplikasiTodoList {
+
+	public static String[] model = new String[10];
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+	// menampilkan todolist
+	public static void showTodoList() {
+
+	}
+
+	// menambah todolist
+	public static void addTodoList() {
+
+	}
+
+	// menghapus todolist
+	public static void removeTodoList() {
+
+	}
+
+
+	// menampilkan view todolist
+	public static void viewShowTodoList() {
+
+	}
+
+	// menampilkan view add todolist
+	public static void viewAddTodoList() {
+
+	}
+
+	// menampilkan view remove todolist
+	public static void viewRemoveTodoList() {
+
+	}
+}
